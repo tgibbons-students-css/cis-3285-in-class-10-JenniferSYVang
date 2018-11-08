32b4f77856c5bf553bc73637791c2c7516e5e5dd
Jennifer Vang <jvang5@css.edu>
2018-11-08 11:05:57 -0600

    Part 1: Trade Adjustment Decorator is complete. All occurence of GBP is now changed to EUR.

---
+++ CurrencyTrader/AdjustTradeDataProvider.cs
@@ -0,0 +1,44 @@
+﻿using CurrencyTrader;
+using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CurrencyTrader
+{
+    public class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        private readonly String url;
+        ITradeDataProvider urlProvider;
+
+        public AdjustTradeDataProvider(String url)
+        {
+            this.url = url;
+            urlProvider = new UrlTradeDataProvider(url);
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<String>();
+            IEnumerable<string> inTradeData;
+            inTradeData = urlProvider.GetTradeData();
+
+            foreach(string line in inTradeData)
+            {
+                if (line.Contains("GBP"))
+                {
+                    tradeData.Add(line.Replace("GBP", "EUR"));
+                }
+                else
+                {
+                    tradeData.Add(line);
+                }
+                
+            }
+            return tradeData;
+        }
+    }
+}
